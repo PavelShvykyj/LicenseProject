4f8026bf4309300937d95889ffeb521f1fd1ef50
Pavel Shvydkyj <myserver1978@gmail.com>
2019-02-15 18:07:12 +0200

    accountig ang auth

--- LicenseApp/ClientApp/src/app/accaunt.service.ts
+++ LicenseApp/ClientApp/src/app/accaunt.service.ts
@@ -1,7 +1,43 @@
+import { UserRoles } from './GlobalEnums';
+import { WebApiService } from './web-api.service';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { ILoginData } from './Interfaces/ILoginData';
 
 @Injectable()
 export class AccauntService {
 
-  constructor() { }
+  private jwtHelper : JwtHelperService = new JwtHelperService();
+
+  constructor(private ApiService : WebApiService) { }
+
+  async Login(loginData : ILoginData) {
+    
+    await this.ApiService.Login(loginData)
+                         .then(response => {})
+                         .catch(error => {});
+    
+
+
+
+
+  } 
+
+  get User() {
+
+    return '';
+  }
+
+  isInRole(role : number) : boolean {
+
+
+
+      return true
+  }
+
+  isLoggedIn() : boolean {
+
+    return true
+}
+
 }
--- LicenseApp/ClientApp/src/app/web-api.service.ts
+++ LicenseApp/ClientApp/src/app/web-api.service.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { ILoginData } from './Interfaces/ILoginData';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -8,7 +9,22 @@ export class WebApiService {
   constructor(private http : HttpClient ) { }
 
   
+  async Login(userData : ILoginData) : Promise<string>  {
 
+    let headers = new HttpHeaders().append('Authorization','none').append('Content-Type','text/json')
+    let connection = this.BASE_URL+"/login";
+    
+    let request = await this.http.post(connection, userData, {
+    headers: headers,
+      observe: 'body',
+      withCredentials: false,
+      reportProgress: false,
+      responseType: 'text'
+    }).toPromise();
+
+    return request;
+
+  }
 
 
 
